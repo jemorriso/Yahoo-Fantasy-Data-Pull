9fda689fbbdf2b878a8a0875b3c75c0fb69a3666
JeMorriso <jeremy.morrison36@gmail.com>
2018-10-20 16:15:49 -0700

    Fix JSON flow into python object, and add method returning teams in league. Started working on grabbing players from those teams

--- pull_yahoo_data.py
+++ pull_yahoo_data.py
@@ -18,21 +18,68 @@ class Yahoo_Data():
         return oauth
 
     def get_league_data(self, url):
-        response = self.oauth.session.get(url, params={'format': 'json'})
+        # returns Oauth "response" object, which I guess must be converted to json.
+        # above is WRONG. rauth utilizes python Requests module, so its a python Requests response object that is being returned.
 
-        print(json.dumps(response.json(), indent=4))
-       # print(r)
-        # pulls only the league info. Look at the JSON output and see how its nested 4x
-        data = pandas.io.json.json_normalize(response.json(), [['fantasy_content', 'leagues', '0', 'league']])
-        self.league = data.T
+        # decodes the response into json format
+        response = self.oauth.session.get(url, params={'format': 'json'})
+        league_json = (json.dumps(response.json(), indent=4))
+        print(league_json)
+
+        # now take the json and turn it into a python object
+        league_object = json.loads(league_json)
+        #print(league_object)
+
+        # grab the relevant data
+        # use pycharm debugger to visualize dict nesting more easily
+        return league_object['fantasy_content']['leagues']['0']['league'][0]
 
     def get_league_teams(self, league_url):
         response = self.oauth.session.get(league_url + "/teams", params={'format': 'json'})
-        print(json.dumps(response.json(), indent=4))
+
+        # decodes the response into json format
+        teams_json = json.dumps(response.json(), indent=4)
+
+        teams_object = json.loads(teams_json)
+        print(teams_object)
+
+        # narrowing in on the relevant content
+        teams_object = teams_object['fantasy_content']['leagues']['0']['league'][1]['teams']
+        print(teams_object)
+
+        teams_list = []
+        # get this bad boy
+        for i in range(0,12):
+            teams_list.append(teams_object[str(i)]['team'][0][2]['name'])
+
+        return teams_list
+
+    # return players in the league
+    def get_league_players(self, league_url):
+        response = self.oauth.session.get(league_url + "/players", params={'format': 'json'})
+        players_json = json.dumps(response.json(), indent=4)
+        print(players_json)
+        players_object = json.loads(players_json)
+        print(players_object)
+
+    # return the players that on each team, storing them as sublists for each team in Yahoo_Data teams attribute
+    def get_team_players(self, league_url):
+
+        # first get the league's players and find out what team they are on
+        players = self.get_league_players(league_url)
+
 
 if __name__=="__main__":
     league_url = 'https://fantasysports.yahooapis.com/fantasy/v2/leagues;league_keys=nhl.l.8681'
+    print("****************************LEAGUE*****************************")
     no_rest_for_fleury = Yahoo_Data(league_url)
-    no_rest_for_fleury.get_league_teams(league_url)
+    #print(no_rest_for_fleury.league)
 
+    print("** ** ** ** ** ** ** ** ** ** ** ** ** ** TEAMS ** ** ** ** ** ** ** ** ** ** ** ** ** ** * ")
+    no_rest_for_fleury.teams = no_rest_for_fleury.get_league_teams(league_url)
 
+    no_rest_for_fleury.get_team_players(league_url)
+
+    # ### testing ###
+    #
+    # response
